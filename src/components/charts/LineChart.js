00db59b68fb5895dcd834ce4a70cc3663f76be33
Sachin <sachinmondal720@gmail.com>
2024-03-07 22:17:29 +0530

    remove warnings

--- src/components/charts/LineChart.js
+++ src/components/charts/LineChart.js
@@ -26,6 +26,7 @@ const LineGraph = () => {
 
     useEffect(() => {
         drawGraph();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [data]);
 
     const drawGraph = () => {
