16485cb3209d752c67d395e70502ac7c92562e94
Sachin <sachinmondal720@gmail.com>
2024-03-27 10:34:28 +0530

    Added some missing files

--- src/apiConfig/apiConfig.js
+++ src/apiConfig/apiConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export const API_BASE_URL = process.env.REACT_APP_BACKEND_URL;
-// export const API_BASE_URL = "http://localhost:5000"
+
 
 export const api = axios.create({
     baseURL: API_BASE_URL,
